6afa3eb6ea4ad25d0417a341507714d63eb8388a
Tim Stephenson <tim@knowprocess.com>
2018-10-09 10:41:18 +0100

    add sample mindmap

---
+++ src/animal-kingdom.mm
@@ -0,0 +1,36 @@
+<!--
+    MindMapper - web based mind mapping tool.
+    Copyright (C) 2018 Tim Stephenson
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+-->
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539076352496" ID="ID_868599390" MODIFIED="1539076361658" TEXT="Animal Kingdom">
+<node CREATED="1539076369666" ID="ID_960657106" MODIFIED="1539076375209" POSITION="right" TEXT="Vertebrates">
+<node CREATED="1539076613410" ID="ID_1851751237" MODIFIED="1539076632539" TEXT="Fish"/>
+<node CREATED="1539076633113" ID="ID_598391800" MODIFIED="1539076635741" TEXT="Mammals"/>
+<node CREATED="1539076636081" ID="ID_167186666" MODIFIED="1539076640956" TEXT="Reptiles"/>
+<node CREATED="1539076641496" ID="ID_177144611" MODIFIED="1539076643948" TEXT="Birds"/>
+<node CREATED="1539076644238" ID="ID_1406214002" MODIFIED="1539076658988" TEXT="Amphibians"/>
+</node>
+<node CREATED="1539076375645" ID="ID_1413957390" MODIFIED="1539076379594" POSITION="right" TEXT="Invertebrates">
+<node CREATED="1539076398833" ID="ID_758496145" MODIFIED="1539076414304" TEXT="Mollusks"/>
+<node CREATED="1539076414925" ID="ID_517909835" MODIFIED="1539076416824" TEXT="Worms"/>
+<node CREATED="1539076417125" ID="ID_1017979791" MODIFIED="1539076448680" TEXT="Arthropods"/>
+<node CREATED="1539076435878" ID="ID_73156563" MODIFIED="1539076442879" TEXT="Echinoderms"/>
+</node>
+</node>
+</map>
